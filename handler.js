cb19ce06b8ec7dce704a218ef385d0e62cd0288d
Blake Danson <blakedanson@gmail.com>
2019-02-22 13:21:51 -0800

    Test fns and clean handler

--- handler.js
+++ handler.js
@@ -2,6 +2,11 @@
 const axios = require('axios');
 
 function fetchFoodTrucks(event) {
+  if (typeof event !== 'object' || Array.isArray(event)){
+    const err = new Error('Function fetchFoodTrucks expects an event as an argument');
+    console.error(err);
+    throw err;
+  }
   return new Promise((resolve,reject)=>{
     axios.get('https://data.sfgov.org/resource/6a9r-agq8.json?status=APPROVED')
     .then((truckList)=>{
@@ -16,7 +21,7 @@ function fetchFoodTrucks(event) {
           input: event,
         })
       }
-      console.log(truckList,' Fetch was successful');
+      console.log('Fetch was successful');
       resolve(resp);
     }).catch((err)=>{
       console.error(err, ' Failed to fetch resource');
@@ -25,13 +30,15 @@ function fetchFoodTrucks(event) {
   });
 }
 
-module.exports.getSFFoodTrucks = async (event, context) => {
+async function getSFFoodTrucks(event, context) {
   try {
     return await fetchFoodTrucks(event);
-    console.log(resp, ' resp finished in getSFFoodTrucks');
     } catch(err) {
       console.error(err);
     };
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+}
+
+module.exports = {
+  getSFFoodTrucks,
+  fetchFoodTrucks,
+}
--- handler.test.js
+++ handler.test.js
@@ -1,7 +1,34 @@
 const expect = require('chai').expect;
+const handler = require('./handler');
+const getSFFoodTrucks = handler.getSFFoodTrucks;
+const fetchFoodTrucks = handler.fetchFoodTrucks;
 
 describe('handler.js',()=>{
   it('should exist',()=>{
-    expect(getFoodTrucks).to.be.a('function');
+    expect(handler).to.be.an('object');
+  });
+  it('should have two functions',()=>{
+    expect(handler).to.have.keys(['getSFFoodTrucks', 'fetchFoodTrucks']);
+  });
+
+  describe('fetchFoodTrucks()',()=>{
+    it('should be a function',()=>{
+      expect(fetchFoodTrucks).to.be.a('function');
+    });
+    it('should accept an event as an argument',()=>{
+      expect(
+        ()=>{
+          fetchFoodTrucks()
+        }
+      ).to.throw('Function fetchFoodTrucks expects an event as an argument');
+    });
+    it('should return a promise',()=>{
+      expect(fetchFoodTrucks({})).to.be.a('promise');
+    });
+    it('should resolve a resp object with an array of message data in the body',(done)=>{
+      fetchFoodTrucks({}).then((resp)=>{
+        expect(JSON.parse(resp.body).message).to.be.an('array');
+      }).then(done,done);
+    });
   });
 });
